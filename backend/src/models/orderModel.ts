aec039478a0c0b81ee059b02a152a0473cfbcc91
Papi Chulo <martinposso33@gmail.com>
2025-01-29 21:41:16 -0500

    feat(Order): Order model and service added

---
+++ backend/src/models/orderModel.ts
@@ -0,0 +1,42 @@
+import mongoose, { Model, Schema, Document } from 'mongoose';
+
+export interface IOrder extends Document {
+  _userID: mongoose.Schema.Types.ObjectId;
+  order_date: Date;
+  service_fee: Number;
+  _discountID: mongoose.Schema.Types.ObjectId;
+  tip: number;
+  total: number;
+}
+
+const OrderSchema: Schema<IOrder> = new Schema<IOrder>({
+  _userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  order_date: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  service_fee: {
+    type: Number,
+    required: true,
+  },
+  _discountID: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  tip: {
+    type: Number,
+    default: 0,
+  },
+  total: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const OrderModel: Model<IOrder> = mongoose.model<IOrder>(
+  'Order',
+  OrderSchema,
+);
